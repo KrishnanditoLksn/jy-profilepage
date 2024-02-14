5baf8a9680285c13d91c0bfa49b4e24eedebc9d7
Gorthdzx <krislaksana123@gmail.com>
2024-02-14 10:27:00 +0700

    feat :added dark mode script

--- src/script/script.js
+++ src/script/script.js
@@ -1,9 +1,16 @@
-const body = document.querySelector('#body-id')
-const darkButton = document.querySelector('#darkButton-id')
+const icon = document.querySelector('.icon');
+icon.style.display = 'none';
 
-darkButton.addEventListener(
-    "click",
-    () => {
-        body.style.backgroundColor = "gray"
-    }
-)
+// const slides = document.querySelectorAll('.item');
+//
+// if (slides.length > 1) {
+//   icon.style.display = 'block';
+// }else{
+//     icon.style.display = 'none';
+// }
+
+let darkMode = () => {
+    /*Memberi warna pada body di html dengan membuat page dark mode */
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-mode');
+}
