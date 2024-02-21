d90b571879dda3d9fc9e2e21aacb2637e8755851
Gorthdzx <krislaksana123@gmail.com>
2024-02-21 09:42:24 +0700

    feat :added color event handler

--- src/script/script.js
+++ src/script/script.js
@@ -1,16 +1,34 @@
 const icon = document.querySelector('.icon');
-icon.style.display = 'none';
+const kegiatanElement = document.getElementById('kegiatan-id');
+const keseruanElement = document.getElementById('keseruan-id');
+const sejarahElement = document.getElementById('sejarah-id');
 
-// const slides = document.querySelectorAll('.item');
-//
-// if (slides.length > 1) {
-//   icon.style.display = 'block';
-// }else{
-//     icon.style.display = 'none';
-// }
+// icon.style.display = 'none';
 
 let darkMode = () => {
     /*Memberi warna pada body di html dengan membuat page dark mode */
     const body = document.querySelector('body');
     body.classList.toggle('dark-mode');
 }
+
+let kegiatanEventHandler = () => {
+    kegiatanElement.addEventListener('click', () => {
+            kegiatanElement.style.backgroundColor = 'red';
+        }
+    );
+}
+
+let keseruanEventHandler = () => {
+    keseruanElement.addEventListener('click', () => {
+            keseruanElement.style.backgroundColor = 'green';
+        }
+    );
+}
+
+let sejarahEventHandler = () => {
+    sejarahElement.addEventListener('click', () => {
+            sejarahElement.style.backgroundColor = 'cyan';
+        }
+    );
+}
+
